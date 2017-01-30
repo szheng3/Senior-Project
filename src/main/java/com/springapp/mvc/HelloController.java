7f06ac610402df2e1b19fd5f4e072635f0e383a7
Shuai Zheng <szheng3@uci.edu>
2017-01-30 10:30:43 -0800

    Add SignIn,AddSignUp

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -5,6 +5,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * Created by Shuai Zheng on 1/29/17.
+ */
+
 @Controller
 public class HelloController {
     //    @RequestMapping(method = RequestMethod.GET)
