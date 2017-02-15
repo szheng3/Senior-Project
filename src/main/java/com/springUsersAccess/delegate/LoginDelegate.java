fff70aa24f75b30fc5a917953df3b4a8d905f84b
Alex Almanza <acalmanz@uci.edu>
2017-02-15 14:21:33 -0800

    Add working hashed login implementation

--- src/main/java/com/springUsersAccess/delegate/LoginDelegate.java
+++ src/main/java/com/springUsersAccess/delegate/LoginDelegate.java
@@ -2,6 +2,7 @@ package com.springUsersAccess.delegate;
 
 import java.sql.SQLException;
 
+import com.springUsersAccess.service.usernames.UsernameService;
 import com.springUsersAccess.service.users.authentication.AuthenticationService;
 
 /**
@@ -19,11 +20,31 @@ public class LoginDelegate {
     public void setAuthenticationService(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
+
+    private PassHashDelegate passHashDelegate;
+    public void setPassHashDelegate(PassHashDelegate passHashDelegate) {
+        this.passHashDelegate = passHashDelegate;
+    }
+
+    private UsernameService usernameService;
+    public void setUsernameService(UsernameService usernameService) {
+        this.usernameService = usernameService;
+    }
     // end of service decelerations
 
-    public boolean isValidUser(String username, String password) throws SQLException {
-        return authenticationService.isValidUser(username, password);
+
+    public boolean isValidUser(String username, String plaintext_password) throws SQLException {
+        System.out.print("Attempting to verify user: " + username + '\n');
+        System.out.print("Plaintext: " + plaintext_password + '\n');
+        if (usernameService.usernameIsTaken(username)) {
+            String hashed_password = passHashDelegate.createHashedPassword(username, plaintext_password);
+            System.out.print("Hashed: " + hashed_password + '\n');
+            return authenticationService.isValidUser(username, hashed_password);
+        }
+        System.out.print("No record of user" + '\n');
+        return false; // The user doesn't exist
     }
+
 
 }
 
