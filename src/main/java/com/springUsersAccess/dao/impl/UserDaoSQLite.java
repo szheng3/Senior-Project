e082c8bd98267c39015fc5477fc65b5b26c5d183
Zeyang Liu <zeyangl1@uci.edu>
2017-02-14 19:27:45 -0800

    new pull from Alex's update
    Alex update 2/14/17

--- src/main/java/com/springUsersAccess/dao/impl/UserDaoSQLite.java
+++ src/main/java/com/springUsersAccess/dao/impl/UserDaoSQLite.java
@@ -3,6 +3,7 @@ package com.springUsersAccess.dao.impl;
 /**
  * Created by Alex Almanza on 1/31/17.
  */
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,42 +11,45 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import com.springUsersAccess.dao.UserDao;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 
 public class UserDaoSQLite implements UserDao {
-    private DataSource dataSource ;
 
-    public DataSource getDataSource() {
-        return this.dataSource;
-    }
-
-    public void setDataSource(DataSource dataSource) {
+    private SingleConnectionDataSource dataSource ;
+    public void setDataSource(SingleConnectionDataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     @Override
     public boolean isUsernameTaken(String username) throws SQLException {
-        String query = "Select count(1) from user where username = ?";
-        PreparedStatement pstmt = dataSource.getConnection().prepareStatement(query);
-        pstmt.setString(1, username);
-        ResultSet resultSet = pstmt.executeQuery();
-        if(resultSet.next())
-            return (resultSet.getInt(1) > 0);
-        else
-            return false;
+         try (Connection connection = dataSource.getConnection()) {
+             String query = "Select count(1) from user where username = ?";
+             PreparedStatement pstmt = connection.prepareStatement(query);
+             pstmt.setString(1, username);
+             ResultSet resultSet = pstmt.executeQuery();
+             if (resultSet.next())
+                 return (resultSet.getInt(1) > 0);
+             else
+                 // TODO: decide if an exception should be thrown when there is no result set available
+                 return false;
+         }
     }
 
     @Override
     public boolean isValidUser(String username, String password) throws SQLException {
-        String query = "Select count(1) from main.user where username = ? and password = ?";
-        PreparedStatement pstmt = dataSource.getConnection().prepareStatement(query);
-        pstmt.setString(1, username);
-        pstmt.setString(2, password);
-        ResultSet resultSet = pstmt.executeQuery();
-        if(resultSet.next())
-            return (resultSet.getInt(1) > 0);
-        else
-            return false;
+        try (Connection connection = dataSource.getConnection()) {
+            String query = "SELECT count(1) FROM main.user WHERE username = ? AND password = ?";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            ResultSet resultSet = pstmt.executeQuery();
+            if (resultSet.next())
+                return (resultSet.getInt(1) > 0);
+            else
+                // TODO: decide if an exception should be thrown when there is no result set available
+                return false;
+        }
     }
 
     @Override
@@ -55,15 +59,13 @@ public class UserDaoSQLite implements UserDao {
             throw new IllegalArgumentException("A record for that username already exists: " + username);
         }
 
-        String query = "INSERT INTO user (" +
-                "username, " +
-                "password) VALUES (" +
-                "?, ?)";
-
-        PreparedStatement pstmt = dataSource.getConnection().prepareStatement(query);
-        pstmt.setString(1, username);
-        pstmt.setString(2, password);
-        pstmt.executeUpdate();
+        try (Connection connection = dataSource.getConnection()) {
+            String query = "INSERT INTO user (username, password) VALUES (?, ?)";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            pstmt.executeUpdate();
+        }
     }
 
     @Override
